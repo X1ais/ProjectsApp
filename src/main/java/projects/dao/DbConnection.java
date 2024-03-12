3f9f44ffcd69343393f012c13968be63dbe92c39
X1ais <chrissxjackson@gmail.com>
2024-03-11 22:43:41 -0600

    first commit

---
+++ src/main/java/projects/dao/DbConnection.java
@@ -0,0 +1,28 @@
+package projects.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import projects.exception.DbException;
+
+public class DbConnection {
+	private static String HOST = "localhost";
+	private static String USER = "projects";
+	private static String PASSWORD = "projects";
+	private static String SCHEMA = "projects";
+	private static int PORT = 3306;
+
+	public static Connection getConnection() {
+		String uri = String.format("jdbc:mysql://%s:%d/%s?user=%s&password=%s&useSSL=false",HOST,PORT,SCHEMA,USER,PASSWORD);
+		try {
+			Connection conn = DriverManager.getConnection(uri);
+			System.out.println("Connection successful!");
+			return conn;
+			
+		} catch (SQLException e) {
+			System.out.println("Connection failed.");
+			throw new DbException(e);
+		}
+	}
+}
